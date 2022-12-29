d77fb5d902692fcb490edf98e506ea3623d9d514
Dang Trung Hieu <hieu-dang@cybozu.vn>
2022-12-29 15:24:04 +0700

    `Config` - Config controller & route

--- index.ts
+++ index.ts
@@ -2,9 +2,10 @@ import express from "express";
 import * as bodyParser from "body-parser";
 import * as path from "path";
 import { assets } from "@shared/assets";
+import { Index } from '@routes/route';
 
 const app = express();
-const router = express.Router();
+// const router = express.Router();
 
 app.set('view engine', 'ejs')
 app.set('views', 'app/views')
@@ -16,15 +17,7 @@ app.locals.xyz = (dirname: string) => {
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
-router.get(
-    "/",
-    async (req: express.Request, res: express.Response): Promise<void> => {
-        res.render("pages/index");
-    }
-);
-
-app.use("/", router);
+app.use("/", Index);
 
 app.listen(8888, () => {
     console.log("listening on port 8888");
