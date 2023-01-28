09672e205d0262d223733e645a4fd043002494cc
Dang Trung Hieu <hieu-dang@cybozu.vn>
2023-01-28 09:21:38 +0700

    `Config` - Install eslint & express session

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const port = process.env.PORT
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'app/views'))
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use("/", Index);
