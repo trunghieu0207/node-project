ea547036c4054b9f3b2bae35665b8b9358be1388
Dang Trung Hieu <hieu-dang@cybozu.vn>
2025-08-13 10:43:39 +0700

    Add demo rule

---
+++ __tests__/invalid/hook-wrong-name.ts
@@ -0,0 +1,7 @@
+// Vi phạm: Tên custom hook sai quy tắc
+export function useFetchUser() {
+  // ...
+}
+export function useCreateOrder() {
+  // ...
+}
---
+++ __tests__/invalid/wrong-naming.ts
@@ -0,0 +1,4 @@
+// Vi phạm: Đặt tên sai quy tắc
+const Api_endpoints = 'url'; // Sai: không UPPER_SNAKE_CASE
+function GetuserData() {} // Sai: không camelCase
+const user_profile = {} // Sai: không PascalCase cho component
